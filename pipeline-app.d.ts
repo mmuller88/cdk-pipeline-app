f4d0321ad5392c07945d26a3e00eddfef784ff14
martin.mueller <m.mueller@unimed.de>
2020-10-05 22:45:58 +0200

    init codepipeline app

--- pipeline-app.d.ts
+++ pipeline-app.d.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { App, AppProps, Construct } from '@aws-cdk/core';
-import { Account } from './accountConfig';
 import { CustomStack } from './custom-stack';
+import { Account } from './accountConfig';
 export interface PipelineAppProps extends AppProps {
     customStack: (scope: Construct, account: Account) => CustomStack;
     branch: string;
