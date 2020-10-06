f184f2678f2437bbe3f2d57f725875ebbd04c223
martin.mueller <m.mueller@unimed.de>
2020-10-06 09:55:16 +0200

    stack name

--- custom-stage.ts
+++ custom-stage.ts
@@ -20,7 +20,7 @@ export class CustomStage extends Stage {
   constructor(scope: Construct, id: string, props: CustomStageProps, account: Account) {
     super(scope, id, props);
 
-    const customStack = props.customStack.call(this, scope, account);
+    const customStack = props.customStack.call(this, this, account);
 
     this.cfnOutputs = customStack.cfnOutputs;
   }
