cb5ade8fe182457e8e88c07ca4f6aff6e551a1d5
martin.mueller <m.mueller@unimed.de>
2020-10-11 13:48:54 +0200

    feat: general accounts and command outputs in tests

--- pipeline-stack.ts
+++ pipeline-stack.ts
@@ -95,7 +95,7 @@ export class PipelineStack extends Stack {
       // tslint:disable-next-line: forin
       for(const cfnOutput in customStage.cfnOutputs){
         useOutputs[cfnOutput] = cdkPipeline.stackOutput(customStage.cfnOutputs[cfnOutput]);
-        useValueOutputs[cfnOutput] = customStage.cfnOutputs[cfnOutput].value;
+        useValueOutputs[cfnOutput] = customStage.cfnOutputs[cfnOutput].value.toString();
       }
 
       preprodStage.addActions(new ShellScriptAction({
