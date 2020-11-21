136982e6ee86c9e231f78b4244cbbcb7aed18699
martin.mueller <m.mueller@unimed.de>
2020-11-21 07:38:11 +0100

    feat: add some docu to pipeline-app

--- pipeline-app.d.ts
+++ pipeline-app.d.ts
@@ -3,10 +3,25 @@ import { App, AppProps, Construct, SecretValue } from "@aws-cdk/core";
 import { CustomStack } from "./custom-stack";
 import { Account, StageAccount } from "./accountConfig";
 export interface PipelineAppProps extends AppProps {
+    /**
+     * The accounts you want to deploy you stack into, managed by the pipeline
+     */
     stageAccounts: StageAccount[];
+    /**
+     * The build account where you want the pipeline to be created. It can have the same account id / region as one of the stage accounts
+     */
     buildAccount: Account;
+    /**
+     * The CDK Stack you would like to create and managed by CDK Pipeline
+     */
     customStack: (scope: Construct, stageAccount: StageAccount) => CustomStack;
+    /**
+     * The git branch you would like to be managed by CDK Pipeline
+     */
     branch: string;
+    /**
+     * The repository name of your repo in GitHub
+     */
     repositoryName: string;
     /**
      * Optional Build Command during the Synth Action
@@ -19,7 +34,15 @@ export interface PipelineAppProps extends AppProps {
         owner: string;
         oauthToken: SecretValue;
     };
+    /**
+     * Manual approval action in the pipeline to approve or reject the CDK deploy.
+     *
+     * @default return is false.
+     */
     manualApprovals?: (stageAccount: StageAccount) => boolean;
+    /**
+     * Run tests after the pipeline was created. You can use CfnOutput here to be available for your tests like 'curl -Ssf $InstancePublicDnsName'
+     */
     testCommands: (stageAccount: StageAccount) => string[];
 }
 export declare class PipelineApp extends App {
--- pipeline-app.ts
+++ pipeline-app.ts
@@ -6,11 +6,25 @@ import { CustomStack } from "./custom-stack";
 import { Account, StageAccount } from "./accountConfig";
 
 export interface PipelineAppProps extends AppProps {
-  // customStage: Stage;
+  /**
+   * The accounts you want to deploy you stack into, managed by the pipeline
+   */
   stageAccounts: StageAccount[];
+  /**
+   * The build account where you want the pipeline to be created. It can have the same account id / region as one of the stage accounts
+   */
   buildAccount: Account;
+  /**
+   * The CDK Stack you would like to create and managed by CDK Pipeline
+   */
   customStack: (scope: Construct, stageAccount: StageAccount) => CustomStack;
+  /**
+   * The git branch you would like to be managed by CDK Pipeline
+   */
   branch: string;
+  /**
+   * The repository name of your repo in GitHub
+   */
   repositoryName: string;
   /**
    * Optional Build Command during the Synth Action
@@ -20,7 +34,15 @@ export interface PipelineAppProps extends AppProps {
    * Access credentials for using the pipe with github. Other git providers are currently not supported.
    */
   gitHub: { owner: string; oauthToken: SecretValue };
+  /**
+   * Manual approval action in the pipeline to approve or reject the CDK deploy.
+   *
+   * @default return is false.
+   */
   manualApprovals?: (stageAccount: StageAccount) => boolean;
+  /**
+   * Run tests after the pipeline was created. You can use CfnOutput here to be available for your tests like 'curl -Ssf $InstancePublicDnsName'
+   */
   testCommands: (stageAccount: StageAccount) => string[];
 }
 
